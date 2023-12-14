550eb873d444ae72ca99f50ac21abf8bc22f4d6e
Clg9100 <chrisgrate@hotmail.com>
2023-12-14 03:11:09 -0500

    Finally able to close ad that was interfering with aeropostale module of scraping.

--- WebScraper/WebScraper.py
+++ WebScraper/WebScraper.py
@@ -1,21 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def gather():
-    driver = webdriver.Chrome()
-
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 5)
     driver.get("https://www.aeropostale.com/search/?q=shirt")
 
     current_url = driver.current_url
 
     driver.implicitly_wait(5)
+
+    driver.switch_to.frame("attentive_creative")
+    driver.implicitly_wait(5)
+    button = driver.find_element(By.ID,"closeIconContainer")
+    button.click()
+    driver.switch_to.default_content()
+   # print("we here")
+   # element = wait.until(EC.presence_of_element_located((By.ID, "closeIconContainer"))).click()
+
+    #This section is for clicking load more until no longer possible
+    """"
     while True:
         try:
-            mark_complete = driver.find_element(By.CLASS_NAME, "load-more-button")
-            mark_complete.click()  # Click the button while there is an option to load more
+            click_more = driver.find_element(By.CLASS_NAME, "load-more-button").click()
+            print("clicked once")
+            driver.implicitly_wait(5)
+            # Click the button while there is an option to load more
         except NoSuchElementException:
-            continue
+            break;
+            
+    """
     driver.implicitly_wait(5)
 
     ##This section prints the price of each entry on the page
